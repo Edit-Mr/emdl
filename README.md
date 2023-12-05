ae846c818a9c6cb587da9d425fd7960ba1d5b7bb
Edit-Mr <elvismao.070512@gmail.com>
2023-12-06 01:30:42 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # worship-download
-輸入歌單即可自動下載 Google Doc 歌譜 PDF，以及 YouTube 音檔
+
+複製歌單後執行即可自動下載 Google Doc 歌譜 PDF，以及 YouTube 音檔
+
+歌譜會以原順序合併成一個 PDF 檔案，音檔會依序下載成 MP3 檔案，其他雲端硬碟檔案會直接下載。
+
+## 可處理的短網址服務
+
+以下為目前實測可處裡的服務商，若有需要可自行新增，或是提出 issue。
+
+- [x] [bit.ly](https://bitly.com/)
+- [x] [tinyurl](https://tinyurl.com/)
+- [x] [piee.tw](https://piee.tw)
+- [x] [pse.is](https://pse.is/)
+- [x] [reurl.cc](https://reurl.cc/)
+<!-- - [ ] [](https://) -->
+
+## 安裝
+
+### 安裝 Python
+
+請先安裝 Python 3.6 以上版本，並確認已經加入環境變數。
+
+### 安裝套件
+
+```bash
+pip install -r requirements.txt
+```
+
+或是
+
+```bash
+pip install pyperclip requests beautifulsoup4 yt_dlp PyPDF2
+```
+
+## 使用
+
+### 執行
+
+```bash
+python main.py
+```
+
+或是雙擊 `執行.bat` 檔案
---
+++ main.py
@@ -0,0 +1,110 @@
+import pyperclip
+import re
+import requests
+from bs4 import BeautifulSoup
+import os
+import yt_dlp
+from PyPDF2 import PdfMerger
+
+# Get text from clipboard
+text = pyperclip.paste()
+
+urls = re.findall(
+    r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+
+redirected_urls = []
+pdfs = []
+for url in urls:
+    response = requests.get(url)
+    if "reurl.cc" in url:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        input_element = soup.find('input', {'id': 'url'})
+        if input_element:
+            value = input_element.get('value')
+            redirected_urls.append(value)
+            print("找到",value)
+    elif "piee.pw" in url:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        a_element = soup.find('p').find('a')
+        if a_element:
+            href = a_element.get('href')
+            redirected_urls.append(href)
+            print("找到",href)
+        else:
+            redirected_urls.append(response.url)
+            print("沒有找到",response.url,"的真實網址")
+    else:
+        redirected_urls.append(response.url)
+        print("找到",response.url)
+
+# Download as PDF or original
+for url in redirected_urls:
+    if "https://docs.google.com/document/" in url:
+        pdf_url = url.split("/edit")[0] + "/export?format=pdf"
+        response = requests.get(pdf_url)
+        if response.status_code == 200:
+            filename = os.path.basename(pdf_url)
+            index = redirected_urls.index(url)
+            filename = f"index{index}.pdf"
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            print(f"成功下載歌譜 {filename}")
+            pdfs.append(filename)
+        else:
+            print(f"Error downloading {url}")
+    elif "https://drive.google.com/file/" in url:
+        # Download as original
+        response = requests.get(url)
+        if response.status_code == 200:
+            filename = os.path.basename(url)
+            index = redirected_urls.index(url)
+            filename = f"index{index}.{filename.split('.')[-1]}"
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            print(f"成功下載檔案 {filename}")
+        else:
+            print(f"檔案下載失敗 {url}")
+    elif "you" in url:
+        ydl_opts = {'extract_flat': 'discard_in_playlist',
+                    'final_ext': 'mp3',
+                    'format': 'bestaudio/best',
+                    'fragment_retries': 10,
+                    'ignoreerrors': 'only_download',
+                    'outtmpl': {'default': '%(title)s.%(ext)s', 'pl_thumbnail': ''},
+                    'postprocessors': [{'key': 'FFmpegExtractAudio',
+                                        'nopostoverwrites': False,
+                                        'preferredcodec': 'mp3',
+                                        'preferredquality': '5'},
+                                       {'add_chapters': True,
+                                        'add_infojson': 'if_exists',
+                                        'add_metadata': True,
+                                        'key': 'FFmpegMetadata'},
+                                       {'already_have_thumbnail': False,
+                                        'key': 'EmbedThumbnail'},
+                                       {'key': 'FFmpegConcat',
+                                        'only_multi_video': True,
+                                        'when': 'playlist'}],
+                    'retries': 5,
+                    'writethumbnail': True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            error_code = ydl.download([url])
+    else:
+        print(f"YouTube 下載失敗 {url}")
+
+pdf_files = [file for file in os.listdir() if file in pdfs]
+
+# Merge PDF files
+merger = PdfMerger()
+for pdf_file in pdf_files:
+    merger.append(pdf_file)
+
+# Save merged PDF as "merged.pdf"
+merger.write("歌譜.pdf")
+merger.close()
+
+# Delete original PDF files
+for pdf_file in pdf_files:
+    os.remove(pdf_file)
+print("已合併歌譜")
